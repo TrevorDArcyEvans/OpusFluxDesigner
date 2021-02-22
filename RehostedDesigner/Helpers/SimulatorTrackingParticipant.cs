2b1fa2649aee16e789ead22101672ee1b7429960
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-02-22 10:50:32 +0000

    - code cleanup

--- RehostedDesigner/Helpers/SimulatorTrackingParticipant.cs
+++ RehostedDesigner/Helpers/SimulatorTrackingParticipant.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Activities;
 using System.Activities.Tracking;
-using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace RehostedWorkflowDesigner.Helpers
